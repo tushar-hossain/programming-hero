5b79eb54164e231124c18665854869e4f3e9f771
Tushar-hossain <tusharhossain455@gmail.com>
2025-02-07 19:11:37 +0600

    practice task solution done

--- MileStone_4_JavaScript/module_2/practice-task/4 nested-friend.js
+++ MileStone_4_JavaScript/module_2/practice-task/4 nested-friend.js
@@ -11,20 +11,19 @@ Note:
 use nested if-else-if-else
 */
 
+let myScore = 81;
 let friendScore = 20;
 
-if (friendScore > 80) {
-  console.log("go for a lunch.");
-} else {
-  if (friendScore < 80) {
-    if (friendScore < 80 && friendScore >= 60) {
-      console.log("good luck next time.");
-    } else if (friendScore < 60 && friendScore >= 40) {
-      console.log("message unseen.");
-    } else if (friendScore < 40) {
-      console.log("block your friend");
-    }
+if (myScore > 80) {
+  if (friendScore > 80) {
+    console.log("go for a lunch.");
+  } else if (friendScore < 80 && friendScore >= 60) {
+    console.log("good luck next time.");
+  } else if (friendScore < 60 && friendScore >= 40) {
+    console.log("keep your friend's message unseen.");
   } else {
-    console.log("go to home and sleep and act sad.");
+    console.log("block your friend");
   }
+} else {
+  console.log("go to home and sleep and act sad.");
 }
