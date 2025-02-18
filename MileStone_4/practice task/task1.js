29081f3cfc5fac667320ef12cfb996b67053d704
Md. Tushar Hossain <85397962+tushar-hossain@users.noreply.github.com>
2025-02-18 11:32:31 +0600

    Delete MileStone_4/practice task directory

--- MileStone_4/practice task/task1.js
+++
@@ -1,9 +0,0 @@
-var taka = 1000;
-var orangeOneKg = 400;
-var appleOneKg = 300;
-
-var totalPrice = orangeOneKg + appleOneKg;
-var outputResult = (taka -= totalPrice);
-
-console.log();
-console.log("Shopkeeper will return money: " + outputResult);
--- MileStone_4/practice task/task3.js
+++
@@ -1,6 +0,0 @@
-var inputNumber = 119;
-var dividedNumber = 5;
-
-var result = inputNumber % dividedNumber;
-
-console.log("Output remainder number is: " + result);
